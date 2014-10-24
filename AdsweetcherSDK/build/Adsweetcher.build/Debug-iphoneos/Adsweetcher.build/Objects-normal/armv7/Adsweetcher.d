15929df9c31d8af762ba6f5cbba53320a85af1ea
Razgon <ivan@adsweetcher.com>
2014-10-23 21:55:10 -0700

    Clean build intermediates

--- AdsweetcherSDK/build/Adsweetcher.build/Debug-iphoneos/Adsweetcher.build/Objects-normal/armv7/Adsweetcher.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ivan/Documents/git/adswitcher/client/iOSLibrary/Adsweetcher/Adsweetcher/Adsweetcher.m \
-  /Users/ivan/Documents/git/adswitcher/client/iOSLibrary/Adsweetcher/Adsweetcher/Adsweetcher.h
